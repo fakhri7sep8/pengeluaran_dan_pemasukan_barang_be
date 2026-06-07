aacc5052e0bfeeac8767358be8e8023491c19d32
fakhri7sep8 <muhammadfakhriramadhan08@gmail.com>
2026-06-07 15:20:30 +0700

    fix: direct connect to tidb cloud with ssl

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { User } from './auth/auth.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { BarangModule } from './barang/barang.module';
@@ -11,19 +10,29 @@ import { PenjualanModule } from './penjualan/penjualan.module';
 
 @Module({
   imports: [
-     ConfigModule.forRoot({
+    ConfigModule.forRoot({
       isGlobal: true,
     }),
 
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: '127.0.0.1',
-      port: 3306,
-      username: 'root',
-      password: 'root',
+      // Langsung tembak host gateway TiDB Cloud lo
+      host: 'gateway01.ap-southeast-1.prod.aws.tidbcloud.com',
+      port: 4000, // Port bawaan TiDB Cloud
+      username: '3pcvfGbj9AQaYDU.root',
+      password: 'Pr7plpVlQiT5cJLn',
       database: 'backend_pkl',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true,
+      
+      // Menggunakan auto load biar gak pusing urusan typo path __dirname
+      autoLoadEntities: true,
+      
+      // Biar tabel baru lo (barang, pembelian, dll) otomatis dibuat di cloud
+      synchronize: true, 
+
+      // WAJIB ADA: Pengaman SSL agar koneksi ke cloud tidak ECONNREFUSED
+      ssl: {
+        rejectUnauthorized: false,
+      },
     }),
     AuthModule,
     BarangModule,
